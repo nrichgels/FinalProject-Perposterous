97a9f101a57a89232ffc93017bfa6ed22b22eb70
jbenda43251 <bendajs@hotmail.com>
2015-04-20 23:00:27 -0500

    Added activities for Credits and instructions

--- PerposterousQuiz/app/src/main/java/com/example/joseph/perposterousquiz/MainActivity.java
+++ PerposterousQuiz/app/src/main/java/com/example/joseph/perposterousquiz/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.joseph.perposterousquiz;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    FrameLayout fragLay;
+    LinearLayout startLay;
+    LinearLayout creditLay;
+    LinearLayout instructionLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragLay = (FrameLayout) findViewById(R.id.fragment_container);
+
+        startLay = (LinearLayout) findViewById(R.id.StartButton);
+        startLay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        creditLay = (LinearLayout) findViewById(R.id.CreditButton);
+        creditLay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newAct = new Intent(MainActivity.this, CreditActivity.class);
+                newAct.putExtra("key","main menu");
+                startActivity(newAct);
+            }
+        });
+
+        instructionLay = (LinearLayout) findViewById(R.id.InstructionButton);
+        instructionLay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newAct = new Intent(MainActivity.this, InstructionActivity.class);
+                newAct.putExtra("key","main menu");
+                startActivity(newAct);
+            }
+        });
+
     }
 
 
